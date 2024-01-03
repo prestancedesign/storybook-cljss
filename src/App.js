f1a0c5b47a06a991b01fc3ffe4a6b0b504ab530a
Michaël Salihi <admin@prestance-informatique.fr>
2024-01-03 23:08:59 +0100

    Initial Webpack React App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+const App = () => {
+  return (
+    <div>
+      <h1 className='heading__one'>React application!!!</h1>
+    </div>
+  );
+};
+
+export default App;
